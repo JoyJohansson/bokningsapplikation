f421aa06c11dd3ff595204922610a77a9427ba7e
k1s3n <christoffer@rehnmark.se>
2024-02-02 22:11:46 +0100

    s

--- SQL Tables, Inserts etc/trigger.sql
+++ SQL Tables, Inserts etc/trigger.sql
@@ -2,6 +2,10 @@ CREATE OR REPLACE FUNCTION check_room_availability()
 RETURNS TRIGGER AS
 $$
 BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Room WHERE Room_ID = NEW.Room_ID) THEN
+        RAISE EXCEPTION 'Room ID does not exist';
+    END IF;
+
     IF EXISTS (
         SELECT 1
         FROM BookingRoom br
@@ -10,15 +14,16 @@ BEGIN
         AND b.Check_out_date > NEW.Check_in_date
         AND b.Check_in_date < NEW.Check_out_date
     ) THEN
-        RAISE EXCEPTION 'Room is already booked for the selected dates';
+        RAISE EXCEPTION 'Room already booked for the selected dates';
     END IF;
+
     RETURN NEW;
 END;
 $$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER prevent_double_booking
-BEFORE INSERT ON BookingRoom
+BEFORE INSERT ON Booking
 FOR EACH ROW
 EXECUTE FUNCTION check_room_availability();
 
--- app.py
+++ app.py
@@ -44,6 +44,19 @@ def k1():
 def error():
     return render_template("error.html")
 
+@app.route("/K2", methods=["POST"])
+def k2():
+    guests = request.form.get("Capacity")
+    error = "För stort sällskap"
+    query = f"SELECT * FROM K2 ORDER BY pricepernight"
+    value = (guests)
+    result = execute_query(query,value,fetch_result=True)
+    
+    if result:
+        return render_template("k1.html", result=result)
+    else:
+        return render_template("error.html",error=error)    
+
 if __name__ == "__main__":
     app.run(debug=True)
 
