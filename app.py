78b23e3e2b0248672fe88b794a54097010bb12e0
JoyJohansson <joycjohansson@gmail.com>
2024-02-13 13:50:25 +0100

    merge

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import base64
 import secrets 
 import random
 import databas
-import databas
 
 
 load_dotenv()
@@ -16,15 +15,14 @@ app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe(16)
 bcrypt = Bcrypt(app)
 
+
 # K1
 @app.route("/")
 def k1():
-    return render_template("k1_start.html")
+    return render_template("k1.html")
 
-def generate_random_code():
-    return random.randint(100000, 999999)
-
-@app.route("/contacts", methods=["GET", "POST"])
+#TODO ändra route
+@app.route("/error", methods=["GET", "POST"])
 def get_room():
     query = """
     SELECT room_id, roomtype, filename, filetype, file_content, capacity, pricepernight
@@ -49,15 +47,14 @@ def get_room():
                 'pricepernight': pricepernight
             }
             converted_results.append(converted_result)
-        return render_template("contacts.html", results=converted_results)
+        return render_template("error.html", results=converted_results)
     else:
-        return render_template("contacts.html", error="No data found")
+        return render_template("error.html", error="No data found")
       
-@app.route("/available_rooms", methods=["POST"])
+
+#TODO mer beskrivande route?
+@app.route("/K2", methods=["POST"])
 def k2():
-    session["start_date"] = request.form["start_date"]
-    session["end_date"] = request.form["end_date"]
-    session["selected_guests"] = request.form["guests"]
     query = """
     SELECT room_id, roomtype, filename, filetype, file_content, capacity, pricepernight
     FROM room_details
@@ -81,9 +78,12 @@ def k2():
                 'pricepernight': pricepernight
             }
             converted_results.append(converted_result)
-        return render_template("k2_available_rooms.html", results=converted_results)
+        return render_template("k2.html", results=converted_results)
     else:
-        return render_template("contacts.html", error="No data found")
+        return render_template("error.html", error="No data found")
+      
+      
+
 
 # email
 @app.route('/email', methods=['GET', 'POST'])
@@ -94,6 +94,7 @@ def email():
         booking_reference = generate_booking_reference()
         return redirect(url_for('bekraftelse', booking_ref=booking_reference))
     return render_template('e-post.html')
+
 
 
 # Bokningsbekräftelse
@@ -112,13 +113,39 @@ def generate_booking_reference():
     # kombinera timestamp med sträng för att skapa unique bokningsreferens
     booking_reference = f'{timestamp}{random_string}'
     return booking_reference
+      
+     
+       # Hämta bokning via bokningsreferens
+@app.route("/get_booking/<booking_reference>", methods=["GET"])
+def get_booking(booking_reference):
+    query = "SELECT * FROM bookings WHERE booking_reference = %s"
+    result = databas.execute_query_fetchone(query, (booking_reference,), fetch_result=True)
 
+    if result:
+        # Returnera bokningsinformation som JSON
+        booking_info = {
+            "booking_reference": result[6],
+            "room_id": result[1],
+        }
+        return jsonify(booking_info)
+    else:
+        return jsonify({"error": "Booking not found"}), 404
+
+  
+def mark_booking_as_cancelled(booking_reference):
+    # Uppdatera databasen för att markera bokningen som avbokad med en timestamp
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    update_query = "UPDATE bookings SET cancelled_at = %s WHERE booking_reference = %s"
+    databas.execute_insert_query(update_query, (timestamp, booking_reference))
+
+
+      
+      
 # Admin registrering
 @app.route("/admin/register", methods=["GET"])
 def admin_register_page():
     return render_template("admin_register_page.html", error=None)
 
-# Admin registrering
 @app.route("/admin/register", methods=["POST"])
 def admin_register():
     if request.method == "POST":
@@ -142,13 +169,14 @@ def admin_register():
 
     return render_template('admin_register_page.html', error=None)
 
+  
 
 # Admin login
 @app.route("/admin/login", methods=["GET"])
 def admin_login_page():  
     return render_template("admin_login_page.html", error=None)
-  
-# Admin login
+
+
 @app.route("/admin/login", methods=["POST"])
 def admin_login():
         username = request.form.get("username")
@@ -189,7 +217,7 @@ def admin_dashboard():
         return render_template('admin_dashboard.html')
     else:
         return redirect(url_for('admin_login_page'))
-      
+
 
 @app.route("/admin/logout", methods=["GET", "POST"])
 def logout():
@@ -203,54 +231,6 @@ def logout():
 def admin_logout_page():
     return render_template('admin_logout_page.html')
 
-@app.route("/room_info", methods=["GET"])
-def room_info():
-    args = request.args
-    room_id = args["room_id"]
-    query = "SELECT Room_ID, Roomtype, PricePerNight FROM room, RoomType WHERE room_id = %s"
-    room = databas.execute_query_fetchone(query, (room_id,), fetch_result=True)
-    print (room)
-    print(room_id)
-    return render_template("k3_room_info.html", room=room)
-
-
-@app.route("/book", methods=["POST"])
-def book_room():
-    args = request.form
-    room_id = args["room_id"]
-    query = "SELECT Room_ID, Roomtype, PricePerNight FROM room, RoomType WHERE room_id = %s"
-    room = databas.execute_query_fetchone(query, (room_id,), fetch_result=True)
-    return render_template("k4_booking_confirmation.html", room=room)
-
-@app.route("/save_booking", methods=["POST"])
-def save_booking():
-    print("save_booking")
-    print(session)
-    room_id = request.form.get("room_id")
-    email = request.form.get("email")
-    name = request.form.get("name")
-    start_date = session.get("start_date")
-    end_date = session.get("end_date")
-    selected_guests = session.get("selected_guests")
-    bookingID = generate_random_code()
-    #TODO guest_id som en autoincrementerad serial
-    #TODO fixa queryn
-    create_guest_query = """INSERT INTO guest_details (name, email)
-                        VALUES (%s, %s)"""
-    guest_saved = databas.execute_insert_query(create_guest_query, (name,email))
-    print(guest_saved)
-    #TODO lägg till room_id i db booking
-    #TODO få denna query att funka
-    query = "SELECT Guest_ID FROM guest_details WHERE name = %s"
-    result = databas.execute_query_fetchone(query,(name,),fetch_result=True)
-    #TODO status som en warchar
-    insert_query = """INSERT INTO booking (booking_id, guest_id,room_id, check_in_date, check_out_date, status)
-                    VALUES (%s,%s, %s,%s,%s, True)"""
-    databas.execute_insert_query(insert_query, (bookingID,result,room_id,start_date, end_date,))
-    #TODO göra en view så vi får upp booking från Databasen
-    query = "SELECT Room_ID, Roomtype, PricePerNight FROM room, RoomType WHERE room_id = %s"
-    room = databas.execute_query_fetchone(query, (room_id,), fetch_result=True)
-    return render_template("k4_booking_confirmation.html", room=room, start_date=start_date,end_date=end_date, selected_guests=selected_guests,name=name,email=email)
 
 if __name__ == "__main__":
     app.run(debug=True)
