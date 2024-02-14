caeac1e789e149152b5cbc5ca2d973bd076beab0
k1s3n <christoffer@rehnmark.se>
2024-02-14 09:31:54 +0100

    added available rooms

--- SQL Tables, Inserts etc/view.sql
+++ SQL Tables, Inserts etc/view.sql
@@ -4,3 +4,19 @@ FROM files
 JOIN room ON files.room_id = room.room_id
 JOIN roomtype ON room.roomtype_id = roomtype.roomtype_id;
 
+
+CREATE VIEW AvailableRooms AS
+SELECT DISTINCT r.Room_ID, rt.roomtype, b.check_in_date, b.check_out_date,f.Filename, f.filetype, f.file_content, rt.capacity, r.pricepernight 
+FROM Room r
+LEFT JOIN Booking b ON r.Room_ID = b.room_id
+LEFT JOIN RoomType rt ON r.RoomType_ID = rt.RoomType_ID
+LEFT JOIN Files f ON r.Room_ID = f.Room_ID
+
+SELECT * FROM availablerooms
+DROP VIEW availablerooms
+
+CREATE VIEW BookedRooms AS
+SELECT DISTINCT r.Room_ID, rt.roomtype, rt.capacity
+FROM Room r
+INNER JOIN Booking b ON r.Room_ID = b.room_id
+LEFT JOIN RoomType rt ON r.RoomType_ID = rt.RoomType_ID
--- app.py
+++ app.py
@@ -59,16 +59,24 @@ def k2():
     session["start_date"] = request.form["start_date"]
     session["end_date"] = request.form["end_date"]
     session["selected_guests"] = request.form["guests"]
+    check_in_date = request.form.get("start_date")
+    check_out_date = request.form.get("end_date")
+    guests = request.form.get("guests")
     query = """
-    SELECT room_id, roomtype, filename, filetype, file_content, capacity, pricepernight
-    FROM room_details
+    SELECT DISTINCT r.Room_ID, rt.roomtype, f.Filename, f.filetype, f.file_content, rt.capacity, r.pricepernight
+    FROM Room r
+    LEFT JOIN Booking b ON r.Room_ID = b.room_id
+    LEFT JOIN RoomType rt ON r.RoomType_ID = rt.RoomType_ID
+    LEFT JOIN Files f ON r.Room_id = f.room_ID
+    WHERE b.room_id IS NULL
+    OR NOT (b.Check_out_date >= %s AND b.Check_in_date <= %s)
     """
-    results = databas.execute_query_fetchall(query, fetch_result=True)
+    results = databas.execute_query_fetchall(query,(check_in_date,check_out_date,), fetch_result=True)
     
     if results:
         converted_results = []
         for result in results:
-            room_id, roomtype, filename, filetype, file_content, capacity, pricepernight = result
+            room_id, roomtype,filename, filetype, file_content, capacity, pricepernight = result
             # Konvertera BYTEA-data till Base64-kodad sträng
             file_content_base64 = base64.b64encode(file_content).decode('utf-8')
             # Lägg till konverterad data till resultatlistan
