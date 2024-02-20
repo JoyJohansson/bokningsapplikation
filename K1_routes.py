73440ee2f41432926c2fb657148442e497078f93
Peewpew <146327649+Peewpew@users.noreply.github.com>
2024-02-20 21:17:59 +0100

    Scripts for dates - Working.

--- K1_routes.py
+++ K1_routes.py
@@ -3,11 +3,13 @@ import app
 import utils
 import databas
 import random
+from datetime import datetime
 
 bp = Blueprint('K1_routes', __name__)
 
 @bp.route("/")
 def k1():
+    current_date = datetime.now().strftime('%Y-%m-%d')
     return render_template("k1_start.html")
 
 
