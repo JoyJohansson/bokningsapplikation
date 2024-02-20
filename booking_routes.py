c77ac5b7a2ce35d9b2da264a3138747001246284
Andreas Pehrsson <apehrsson@hotmail.com>
2024-02-20 20:12:41 +0100

    total price

--- booking_routes.py
+++ booking_routes.py
@@ -54,7 +54,7 @@ def save_booking():
     num_days = (end_date - start_date).days
 
 
-    utils.total_price = num_days * room_price
+    total_price = num_days * room_price
 
     
     #Insert för att lägga till namn och emil i guest_details tabellen
@@ -93,7 +93,7 @@ def save_booking():
             #TEST FÖR BACKEND FÖR ATT SE SÅ DEN TAR MED ALLA VARIABLAR
             print(f"Option ID för '{option_value}' hittades inte i databasen.")
     
-    return render_template("k4_booking_confirmation.html", booking_ID=booking_ID, room=room, start_date=start_date, end_date=end_date, selected_guests=selected_guests, name=name, email=email)
+    return render_template("k4_booking_confirmation.html", booking_ID=booking_ID, room=room, start_date=start_date, end_date=end_date, selected_guests=selected_guests, name=name, email=email, total_price=total_price)
 
 @bp.route("/guest/booking", methods=["GET"])
 def guest_booking():
